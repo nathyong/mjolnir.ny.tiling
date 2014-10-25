57f028628247f9154ee21198ac4041c7912f7699
nathyong <nathyong@gmail.com>
2014-10-25 13:48:53 +1100

    Add documentation and simplify function calls.

--- spaces.lua
+++ spaces.lua
@@ -7,8 +7,13 @@ local keycodes = require "mjolnir.keycodes"
 
 spaces.modifiers = {ctrl = true}
 
--- 'key' is actually the string of whatever key you have bound to press to get
--- to a particular space.
+--- spaces.movetospace(key)
+--- Function
+---
+--- 'key' should be a string describing the key to press to move to a space.
+---
+--- Move to a particular space by simulating the key events needed to move to
+--- that space, bringing a window along with it if there is one.
 function spaces.movetospace(key)
     local frame = window.focusedwindow():frame()
 
@@ -16,17 +21,23 @@ function spaces.movetospace(key)
     local position = {x=frame.x + 65, y=frame.y + 7}
     event.newmouseevent(event.types.mousemoved, position, 'left'):post()
     event.newmouseevent(event.types.leftmousedown, position, 'left'):post()
-    local kev = event.newkeyevent({}, '', true)
-    kev:setkeycode(keycodes.map[key])
-    kev:setflags(spaces.modifiers)
-    kev:post()
-    kev = event.newkeyevent({}, '', false)
-    kev:setkeycode(keycodes.map[key])
-    kev:setflags(spaces.modifiers)
-    kev:post()
+    newkeyevent(spaces.modifiers, key, true):post()
+    newkeyevent(spaces.modifiers, key, false):post()
     event.newmouseevent(event.types.leftmousedown, position, 'left'):post()
     event.newmouseevent(event.types.leftmouseup, position, 'left'):post()
     event.newmouseevent(event.types.mousemoved, position0, 'left'):post()
+end
+
+
+--- newkeyevent(modifiers, key, ispressed) -> event
+--- Function
+---
+--- Acts like mjolnir._asm.eventtap.event.newkeyevent(...), but actually works.
+function newkeyevent(modifiers, key, ispressed)
+  local kev = event.newkeyevent({}, '', ispressed)
+  kev:setkeycode(keycodes.map[key])
+  kev:setflags(modifiers)
+  return kev
 end
 
 return spaces
